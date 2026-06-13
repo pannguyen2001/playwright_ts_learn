5961671ecfe310f17ae94d86445a720bac615202
panpan2001 <truonghoc19102001@gmail.com>
2026-06-13 20:56:40 +0700

    update code

--- main.ts
+++ main.ts
@@ -2,17 +2,19 @@ import {
   BASE_URL,
   DASHBOARD_URL,
   USER_NAME,
-  PASSWORD
+  PASSWORD,
 } from "./configs/constants";
 
-console.log(BASE_URL, DASHBOARD_URL, USER_NAME, PASSWORD)
+console.log(BASE_URL, DASHBOARD_URL, USER_NAME, PASSWORD);
 
 import moment from "moment-timezone";
 
 function timestamp(): string {
-  return moment.tz(new Date(), "Asia/Ho_Chi_Minh").format('YYYY-MM-DD HH-mm-ss')
+  return moment
+    .tz(new Date(), "Asia/Ho_Chi_Minh")
+    .format("YYYY-MM-DD HH-mm-ss");
 }
 
-console.log(timestamp())
-const val = 2
-console.log(val.toString().padStart(2, '0'))
+console.log(timestamp());
+const val = 2;
+console.log(val.toString().padStart(2, "0"));
