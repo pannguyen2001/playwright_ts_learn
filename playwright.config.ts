e5f662073f5a4bdb9c9e8878b166124b5657d8b1
panpan2001 <truonghoc19102001@gmail.com>
2026-06-25 20:31:16 +0700

    setup ci-cd

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,21 +12,21 @@ import { defineConfig, devices } from "@playwright/test";
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-	testDir: "./tests",
-	testMatch: "**/*.spec.ts", // testMatch: '**/*.spec.ts',
-	/* Run tests in files in parallel */
-	fullyParallel: true,
-	/* Fail the build on CI if you accidentally left test.only in the source code. */
-	forbidOnly: !!process.env.CI,
-	/* Retry on CI only */
-	retries: process.env.CI ? 2 : 0, // retry: 2,
-	/* Opt out of parallel tests on CI. */
-	workers: process.env.CI ? 1 : undefined, // workers: 4,
-	// repeat-each: 5, // repeat-each: 5, for known flasy test
-	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
-	reporter: [
-		["list"], // Xem ở terminal
-		["html"], // Xem debug nhanh
+  testDir: "./tests",
+  testMatch: "**/*.spec.ts", // testMatch: '**/*.spec.ts',
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0, // retry: 2,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined, // workers: 4,
+  // repeat-each: 5, // repeat-each: 5, for known flasy test
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: [
+    ["list"], // Xem ở terminal
+    ["html"], // Xem debug nhanh
 
     // 👇 Cấu hình Allure ở đây
     ["allure-playwright", { resultsDir: "allure-results" }],
