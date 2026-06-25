0391cd98cace16c0e027f1c08456fc849ddec1da
panpan2001 <truonghoc19102001@gmail.com>
2026-06-25 20:15:47 +0700

    setup ci-cd

--- utils/log4js.ts
+++ utils/log4js.ts
@@ -12,42 +12,42 @@ const now = moment(new Date()).format(DATE_FORMAT);
 
 const logFolderPath = LOG_FOLDER_PATH || "logs";
 if (!fs.existsSync(logFolderPath))
-	fs.mkdirSync(logFolderPath, { recursive: true });
+  fs.mkdirSync(logFolderPath, { recursive: true });
 
 const worker = process.env.TEST_WORKER_INDEX ?? "0";
 
-const logFileName = `${logFolderPath}/${now}_worker-${worker}.log`;
+const logFileName = `${logFolderPath}/${now}/worker-${worker}.log`;
 
 log4js.configure({
-	appenders: {
-		out: {
-			type: "stdout",
-			layout: {
-				type: "pattern",
-				pattern:
-					"[\x1b[1m%[%p%]\x1b[0m][\x1b[36m%d{yyyy-MM-dd hh:mm:ss}\x1b[0m][\x1b[35m%f{1}:%M:%l:%o\x1b[0m]%n\x1b[1m%[%m%]\x1b[0m",
-			},
-		},
-		app: {
-			type: "file",
-			filename: logFileName,
-			maxLogSize: 10485760,
-			numBackups: 3,
-			layout: {
-				type: "pattern",
-				// pattern: "[%p][%d{yyyy-MM-dd hh:mm:ss}][%f{1}:%M:%l] %m%n",
-				pattern: "[%p][%d{yyyy-MM-dd hh:mm:ss}][%f{1}:%M:%l]%n%m",
-			},
-		},
-	},
-	categories: {
-		default: {
-			appenders: ["out", "app"],
-			level: "debug",
-			enableCallStack: true,
-		},
-		// app: { appenders: ["app"], level: "trace"},
-	},
+  appenders: {
+    out: {
+      type: "stdout",
+      layout: {
+        type: "pattern",
+        pattern:
+          "[\x1b[1m%[%p%]\x1b[0m][\x1b[36m%d{yyyy-MM-dd hh:mm:ss}\x1b[0m][\x1b[35m%f{1}:%M:%l:%o\x1b[0m]%n\x1b[1m%[%m%]\x1b[0m",
+      },
+    },
+    app: {
+      type: "file",
+      filename: logFileName,
+      maxLogSize: 10485760,
+      numBackups: 3,
+      layout: {
+        type: "pattern",
+        // pattern: "[%p][%d{yyyy-MM-dd hh:mm:ss}][%f{1}:%M:%l] %m%n",
+        pattern: "[%p][%d{yyyy-MM-dd hh:mm:ss}][%f{1}:%M:%l]%n%m",
+      },
+    },
+  },
+  categories: {
+    default: {
+      appenders: ["out", "app"],
+      level: "debug",
+      enableCallStack: true,
+    },
+    // app: { appenders: ["app"], level: "trace"},
+  },
 });
 
 const logger = log4js.getLogger();
@@ -55,5 +55,5 @@ export default logger;
 
 /** Factory — each file/class gets its own named logger */
 export function createLogger(context: string): Logger {
-	return log4js.getLogger(context);
+  return log4js.getLogger(context);
 }
